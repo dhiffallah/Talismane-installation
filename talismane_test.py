9fe54cf4b8717a63ac54f7c014c16bb39baef558
Dhifallah OTHMEN <dhifallah.othmen@caselawanalytics.com>
2020-01-05 19:06:02 +0100

    Add tests for talismane

--- talismane_test.py
+++ talismane_test.py
@@ -1,26 +1,17 @@
 import talismane_built_structure as tbs
 import talismane_analysis as tal
-from collections import Counter
 import sys
 
 
-def extract_keywords(document):
+def display_token_features(document):
     """
-    Extract keywords from contrcat using
-    "Talismane"
+    display  talisamane token features 
+    
     """
-    key_words = []
     for sent in document.sents:
         for token in sent.tokens:
-            #print("ID:" ,token.id ,"  Word: ",token.word ," Pos: ", token.pos ," Dep: ", token.dep ,"Dependants: ", token.get_dependants() , " Distance: ", token.distance(token.word) , " Get group : ",token.get_group() )
-            if (token.pos == "NC" and (token.dep == 'prep' or token.dep == "obj" or token.dep == "dep_coord") and
-                    (token.head.pos == "P" or token.head.pos == "VINF" or token.head.pos == "P+D"or token.head.pos == "CC") or
-                    ((token.word == "VPP" or token.word == "V" or token.word == "VINF") and (token.dep == "obj" or token.dep == "sub" or token.dep == "root") and
-                     (token.head.pos == "V" or token.head.pos == "CS" or token.head.pos == "root" or token.head.pos == "VPR") or
-                     (token.pos == "ADJ" and token.dep == 'mod' and token.head.pos == "NC")) and (token.is_number() == False) and (token.is_currency() == False)):
-                key_words.append(token.word)
-    key_words = Counter(key_words)
-    return key_words
+            print("ID: " ,token.id ,"  Word: ",token.word ," Pos: ", token.pos ," Dep: ", token.dep ,"Dependants: ", token.get_dependants() , " Distance : ", token.distance(token.word) , " Get group : ",token.get_group() )
+            
 
 
 def extract_chunk(document):
@@ -28,11 +19,8 @@ def extract_chunk(document):
     Extract Groups Chunks :
     A chunk  can be : SUJ, OBJ, VRB, CONT, UNK
     """
-    list_groups = list()
-    raw_text = document.raw
     for sent in document.sents:
-        print("*****************LOAD CHUNK*********************")
-        print("Talismane chunk : ", sent.chunk())
+        print("Talismane chunks : ", sent.chunk())
 
 
 if __name__ == '__main__':
@@ -42,5 +30,5 @@ if __name__ == '__main__':
             path_to_output="data/talismane_output.txt")
         print("*****************FILE PARSED*****************")
     docs = tbs.doc_from_file("data/talismane_output.txt")
-    print(extract_keywords(docs))
+    print(display_token_features(docs))
     print(extract_chunk(docs))
